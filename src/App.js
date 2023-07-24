ea6e53107303db15cb02bfffd6508296a597b829
Mariana Téllez <tellezresendizmariana@gmail.com>
2023-07-24 12:57:18 -0600

    Updated footer with GitHub repository

--- src/App.js
+++ src/App.js
@@ -4,6 +4,28 @@ function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>
+        <p>
+          Designed and coded by{" "}
+          <a
+            href="www.linkedin.com/in/mariana-tellez-resendiz-134354232"
+            target="_blank"
+            rel="noreferrer"
+            title="LinkedIn"
+          >
+            Mariana Tellez Resendiz
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/MarianaTellezR/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+            title="GitHub Repository"
+          >
+            open-sourced on GitHub
+          </a>
+        </p>
+      </footer>
     </div>
   );
 }
