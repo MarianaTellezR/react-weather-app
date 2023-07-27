692c6e0ca7b4a66eb6171d3a6ffdd6e23937ad8b
Mariana Téllez <tellezresendizmariana@gmail.com>
2023-07-27 14:51:12 -0600

    Added Bootstrap and created Weather.js file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <Weather />
       <footer>
         <p>
           Designed and coded by{" "}
