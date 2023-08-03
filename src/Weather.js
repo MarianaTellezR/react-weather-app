dc362e40157cebc7c04e64a5aea9668e4b3caea0
Mariana Téllez <tellezresendizmariana@gmail.com>
2023-08-03 11:23:06 -0600

    Created Forecast Section

--- src/Weather.js
+++ src/Weather.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
 import FormattedDate from "./FormattedDate";
+import WeatherForecast from "./WeatherForecast";
 
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
@@ -70,128 +71,138 @@ export default function Weather(props) {
     return (
       <div className="Weather">
         {/* Background Cool Section */}
-        <div
-          className="upper-section-main"
-          style={{ backgroundImage: `url(${urlBackground})` }}
-        >
-          <form onSubmit={handleSubmit}>
-            <div className="row">
-              <div className="col-12 col-sm-9">
-                <input
-                  type="search"
-                  placeholder="Enter a city"
-                  className="form-control"
-                  autoFocus="on"
-                  onChange={handleCityChange}
+        <div className="row">
+          <div className="col-12 col-md-9">
+            <div
+              className="upper-section-main"
+              style={{ backgroundImage: `url(${urlBackground})` }}
+            >
+              <form onSubmit={handleSubmit}>
+                <div className="row">
+                  <div className="col-12 col-sm-9">
+                    <input
+                      type="search"
+                      placeholder="Enter a city"
+                      className="form-control"
+                      autoFocus="on"
+                      onChange={handleCityChange}
+                    />
+                  </div>
+                  <div className="col-12 col-sm-3">
+                    <input
+                      type="submit"
+                      value="Search"
+                      className="btn btn-light btn-search"
+                    />
+                  </div>
+                </div>
+              </form>
+
+              <div className="city-title-section">
+                <img
+                  className="location-icon"
+                  alt="City"
+                  src={require("./img/location.png")}
                 />
+                <h2>{weatherData.city}</h2>
               </div>
-              <div className="col-12 col-sm-3">
-                <input
-                  type="submit"
-                  value="Search"
-                  className="btn btn-light btn-search"
-                />
-              </div>
-            </div>
-          </form>
 
-          <div className="city-title-section">
-            <img
-              className="location-icon"
-              alt="City"
-              src={require("./img/location.png")}
-            />
-            <h2>{weatherData.city}</h2>
-          </div>
+              <h3>
+                <FormattedDate date={weatherData.date} />
+              </h3>
 
-          <h3>
-            <FormattedDate date={weatherData.date} />
-          </h3>
-
-          <div className="row weather-results">
-            <div className="col-xs-12 col-sm-6 weather-temperature-info">
-              <img
-                className="weather-now"
-                alt={weatherData.description}
-                src={require("./img/" + icon + ".png")}
-              />
-              <h4 className="text-capitalize">{weatherData.description}</h4>
-            </div>
-            <div className="col-xs-12 col-sm-6 weather-temperature">
-              <h1>
-                {Math.round(weatherData.temperature)} <sup>°C</sup>
-              </h1>
-            </div>
-          </div>
-        </div>
-        {/* Below Background Section */}
-        <div
-          className="lower-section-main"
-          style={{ backgroundColor: colorBackground }}
-        >
-          <div className="row">
-            <div className="col-12 col-sm-4 weather-feels">
-              <div className="row">
-                <div className="col-12 col-sm-6 feels-like-left">
+              <div className="row weather-results">
+                <div className="col-xs-12 col-sm-6 weather-temperature-info">
                   <img
                     className="weather-now"
-                    alt="Today's Weather"
+                    alt={weatherData.description}
                     src={require("./img/" + icon + ".png")}
                   />
+                  <h4 className="text-capitalize">{weatherData.description}</h4>
                 </div>
-                <div className="col-12 col-sm-6 feels-like-right">
-                  <p>
-                    Feels like: <br></br>
-                    {Math.round(weatherData.feels)} °C
-                  </p>
+                <div className="col-xs-12 col-sm-6 weather-temperature">
+                  <h1>
+                    {Math.round(weatherData.temperature)} <sup>°C</sup>
+                  </h1>
                 </div>
               </div>
             </div>
-            <div className="col-12 col-sm-4 weather-stats">
-              <ul>
-                <li>
-                  <img
-                    className="weather-stats-icon"
-                    alt="Humidity"
-                    src={require("./img/humidity.png")}
-                  />
-                  {weatherData.humidity} %
-                </li>
-                <li>
-                  <img
-                    className="weather-stats-icon"
-                    alt="Wind Speed"
-                    src={require("./img/wind.png")}
-                  />
-                  {Math.round(weatherData.wind)} km/h
-                </li>
-                <li>
-                  <img
-                    className="weather-stats-icon"
-                    alt="Visibility"
-                    src={require("./img/visibility.png")}
-                  />
-                  {Math.round(weatherData.visibility / 1000)} km
-                </li>
-              </ul>
-            </div>
-            <div className="col-12 col-sm-4 weather-max-min">
+            {/* Below Background Section */}
+            <div
+              className="lower-section-main"
+              style={{ backgroundColor: colorBackground }}
+            >
               <div className="row">
-                <div className="col-12 col-sm-6 max-min-temperature-left">
-                  <img
-                    className="weather-max-min-icon"
-                    alt="Maximum and Minimum Temperature"
-                    src={require("./img/termometer.png")}
-                  />
+                <div className="col-12 col-sm-4 weather-feels">
+                  <div className="row">
+                    <div className="col-12 col-sm-6 feels-like-left">
+                      <img
+                        className="weather-now"
+                        alt="Today's Weather"
+                        src={require("./img/" + icon + ".png")}
+                      />
+                    </div>
+                    <div className="col-12 col-sm-6 feels-like-right">
+                      <p>
+                        Feels like: <br></br>
+                        {Math.round(weatherData.feels)} °C
+                      </p>
+                    </div>
+                  </div>
                 </div>
-                <div className="col-12 col-sm-6 max-min-temperature-right">
+                <div className="col-12 col-sm-4 weather-stats">
                   <ul>
-                    <li>Max: {Math.round(weatherData.maxtemp)}°C</li>
-                    <li>Min: {Math.round(weatherData.mintemp)}°C</li>
+                    <li>
+                      <img
+                        className="weather-stats-icon"
+                        alt="Humidity"
+                        src={require("./img/humidity.png")}
+                      />
+                      {weatherData.humidity} %
+                    </li>
+                    <li>
+                      <img
+                        className="weather-stats-icon"
+                        alt="Wind Speed"
+                        src={require("./img/wind.png")}
+                      />
+                      {Math.round(weatherData.wind)} km/h
+                    </li>
+                    <li>
+                      <img
+                        className="weather-stats-icon"
+                        alt="Visibility"
+                        src={require("./img/visibility.png")}
+                      />
+                      {Math.round(weatherData.visibility / 1000)} km
+                    </li>
                   </ul>
                 </div>
+                <div className="col-12 col-sm-4 weather-max-min">
+                  <div className="row">
+                    <div className="col-12 col-sm-6 max-min-temperature-left">
+                      <img
+                        className="weather-max-min-icon"
+                        alt="Maximum and Minimum Temperature"
+                        src={require("./img/termometer.png")}
+                      />
+                    </div>
+                    <div className="col-12 col-sm-6 max-min-temperature-right">
+                      <ul>
+                        <li>Max: {Math.round(weatherData.maxtemp)}°C</li>
+                        <li>Min: {Math.round(weatherData.mintemp)}°C</li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
+          </div>
+          <div
+            className="col-12 col-md-3 forecast-section"
+            style={{ backgroundColor: colorBackground }}
+          >
+            <WeatherForecast />
           </div>
         </div>
       </div>
