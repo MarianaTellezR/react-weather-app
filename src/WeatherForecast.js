4fe49d0020c182697732b71dbadd928f438cc65e
Mariana Téllez <tellezresendizmariana@gmail.com>
2023-08-03 11:54:58 -0600

    Started layout of weather forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -4,7 +4,25 @@ import "./WeatherForecast.css";
 export default function WeatherForecast() {
   return (
     <div className="WeatherForecast">
-      <p>Forecast 1</p>
+      <div className="row">
+        <p>Friday</p>
+        <div className="col-12 icon-section">
+          <img
+            className="icon-forecast"
+            alt="Forecast"
+            src={require("./img/01d.png")}
+          />
+        </div>
+        <div className="col-12 description-section">
+          <p>Light Rain</p>
+        </div>
+        <div className="col-12 temperature-section">
+          <ul>
+            <li>19 °C</li>
+            <li>16 °C</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
