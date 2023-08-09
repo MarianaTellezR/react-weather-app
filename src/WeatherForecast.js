672bee07ceda0000574adbb7ce2f5379f6e7c9cf
Mariana Téllez <tellezresendizmariana@gmail.com>
2023-08-09 10:45:34 -0600

    Updated WeatherForecast.js with 5 day forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -20,86 +20,18 @@ export default function WeatherForecast(props) {
         <h3>WEATHER FORECAST</h3>
         {/* Day 1 */}
         <WeatherForecastDay data={forecast[0]} />
-
         <hr />
 
-        {/* Day 2 */}
-        <div className="row forecast-section">
-          <p className="day-forecast">Saturday</p>
-          <div className="col-12 col-md-6 left-forecast">
-            <img
-              className="icon-forecast"
-              alt="Forecast"
-              src={require("./img/02d.png")}
-            />
-            <p className="text-capitalize">Light Rain</p>
-          </div>
-          <div className="col-12 col-md-6 right-forecast">
-            <p>
-              16° <strong>19°</strong>
-            </p>
-          </div>
-        </div>
-
+        <WeatherForecastDay data={forecast[1]} />
         <hr />
 
-        {/* Day 3 */}
-        <div className="row forecast-section">
-          <p className="day-forecast">Sunday</p>
-          <div className="col-12 col-md-6 left-forecast">
-            <img
-              className="icon-forecast"
-              alt="Forecast"
-              src={require("./img/02d.png")}
-            />
-            <p className="text-capitalize">Light Rain</p>
-          </div>
-          <div className="col-12 col-md-6 right-forecast">
-            <p>
-              16° <strong>19°</strong>
-            </p>
-          </div>
-        </div>
-
+        <WeatherForecastDay data={forecast[2]} />
         <hr />
 
-        {/* Day 4 */}
-        <div className="row forecast-section">
-          <p className="day-forecast">Monday</p>
-          <div className="col-12 col-md-6 left-forecast">
-            <img
-              className="icon-forecast"
-              alt="Forecast"
-              src={require("./img/02d.png")}
-            />
-            <p className="text-capitalize">Light Rain</p>
-          </div>
-          <div className="col-12 col-md-6 right-forecast">
-            <p>
-              16° <strong>19°</strong>
-            </p>
-          </div>
-        </div>
-
+        <WeatherForecastDay data={forecast[3]} />
         <hr />
 
-        {/* Day 5*/}
-        <div className="row forecast-section">
-          <p className="day-forecast">Tuesday</p>
-          <div className="col-12 col-md-6 left-forecast">
-            <img
-              className="icon-forecast"
-              alt="Forecast"
-              src={require("./img/02d.png")}
-            />
-            <p className="text-capitalize">Light Rain</p>
-          </div>
-          <div className="col-12 col-md-6 right-forecast">
-            <p>
-              16° <strong>19°</strong>
-            </p>
-          </div>
-        </div>
+        <WeatherForecastDay data={forecast[4]} />
       </div>
     );
   } else {
